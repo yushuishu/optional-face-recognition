65d38e876a24f29f8b359154900f8ddaaf2ff0e4
谁书-ss <yushuishu@qq.com>
2023-07-27 22:32:31 +0800

    feat:optional face recognition

---
+++ src/main/java/com/shuishu/face/OptionalFaceRecognitionApplication.java
@@ -0,0 +1,23 @@
+package com.shuishu.face;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+/**
+ * @Author ：谁书-ss
+ * @Date   ： 2023-07-27 22:23
+ * @IDE    ：IntelliJ IDEA
+ * @Motto  ：ABC(Always Be Coding)
+ * <p></p>
+ * @Description ：
+ */
+@SpringBootApplication
+public class OptionalFaceRecognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptionalFaceRecognitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shuishu/face/OptionalFaceRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuishu.face;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionalFaceRecognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
